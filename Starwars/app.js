6c09c1b48bedea0b4918a2c310bc1ae21d580722
cody.b.gordon@gmail.com <cody.b.gordon@gmail.com>
2016-09-29 23:05:18 -0700

    starwars

--- Starwars/app.js
+++ Starwars/app.js
@@ -1,41 +1,81 @@
-(function () {
-
-    let startUrl = 'http://swapi.co/api/people/?format=json';
+//(function () {
+    let peopleUrl = 'http://swapi.co/api/people/?format=json';
+    let planetUrl = 'http://swapi.co/api/planets/?format=json';
     let characters = [];
+    let planetArray = []
+
+    console.log(characters)
+    console.log(planetArray)
+
+    function buildPlanet(){
+
+                 $('#field').empty()
+            $('#field').append('<table class="table table-inverse"><thead><tr><th>Name</th><th>climate</th><th>terrain</th></tr></thead><tbody id=tbody></tbody></table>')
+
+        planetArray.forEach(function (data) {
+            $('#tbody').append('<tr><td>' + data.name + '</td><td>' + data.climate + '</td><td>' + data.terrain + '</td></tr>')
 
 
-    function runIt() {
+        });
+
+    }
+
+    function buildPeople() {
+            $('#field').empty()
+            $('#field').append('<table class="table table-inverse"><thead><tr><th>Name</th><th>DOB</th><th>Height</th></tr></thead><tbody id=tbody></tbody></table>')
+
         characters.forEach(function (data) {
-            console.log(data.name)
+            $('#tbody').append('<tr><td>' + data.name + '</td><td>' + data.birth_year + '</td><td>' + data.height + '</td></tr>')
+
+
         });
     }
-console.log(characters)
-    for (i = 0; i <= characters.length; i++) {
-        console.log()
+
+    getCharacters(peopleUrl);
+    getPlanet(planetUrl)
+
+
+    function getPlanet(url) {
+
+        getWorld(url, function (planet) {
+            planet.results.forEach(function (world) {
+                planetArray.push(world);
+            });
+            if (planet.next) {
+                getPlanet(planet.next);
+            }
+            else{
+            //buildPlanet()
+            console.log(planet)
+            }
+        });
     }
-
-    getCharacters(startUrl);
-
-    console.log(characters[0]);
 
     function getCharacters(url) {
 
         getPeople(url, function (people) {
             people.results.forEach(function (person) {
                 characters.push(person);
-                //runIt()
             });
 
             if (people.next) {
                 getCharacters(people.next);
             }
+            else{
+            //buildPeople()
+            }
         });
     }
 
+
+    function getWorld(url, callback) {
+  $.getJSON(url, callback);
+    }
     function getPeople(url, callback) {
 
         $.getJSON(url, callback);
+
     }
 
 
-})();
+//})();
